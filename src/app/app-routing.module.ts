440fa66dd5d55ebe9e5ec77989fef7010629dffd
Georgii Klimov <klimovgg@gmail.com>
2022-08-21 20:41:31 +0200

    adjust main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainPageComponent} from "./pages/main/main-page.component";
-import {ButtonPrimaryComponent} from "./shared/components/button/buttons/button-primary/button-primary.component";
+import {ButtonPrimaryComponent} from "./shared/components/buttons/button-primary/button-primary.component";
 
 const routes: Routes = [
   {path: '', component: MainPageComponent},
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,ts}",],
   theme: {
     extend: {
+      maxWidth: {
+        'layout-lg': '1176px',
+        'wrapper': '73.5rem'
+      },
+      maxHeight:{
+        'wrapper': '51.5rem'
+      },
+      height: {
+        'wrapper': '51.5rem'
+      },
       boxShadow: {
         'primary-btn': '0 2px 20px rgba(59, 171, 54, 0.54)',
         'secondary-btn': '0 2px 20px rgba(59, 171, 54, 0.54)'
@@ -16,9 +26,9 @@ module.exports = {
         black: '#060D00',
         white: '#FBFBFB',
         grey: {
-          light: '#9E9E9E',
+          light: '#F2F2F2',
           main: '#525252',
-          dark: '#F2F2F2'
+          dark: '#9E9E9E'
         },
       },
     },
