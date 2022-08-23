8cf91d12ee65d91c0a87f98e4ad7b20dbb266e07
Georgii Klimov <klimovgg@gmail.com>
2022-08-23 21:23:41 +0200

    adjust input component

---
+++ src/app/shared/components/form-components/input-components/input-text/input-text.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import {InputBaseComponent} from "../../base/input-base/input-base.component";
+
+@Component({
+  selector: 'app-input-text',
+  templateUrl: './input-text.component.html',
+  styleUrls: ['./input-text.component.scss']
+})
+export class InputTextComponent extends InputBaseComponent {
+
+}
