c57f1b32fa45d0c2e7addbf438c0f195a8d86fa3
Georgii Klimov <klimovgg@gmail.com>
2022-08-28 02:24:33 +0200

    adjust header component

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -4,10 +4,11 @@ import { CoreComponent } from './components/core/core.component';
 import { SharedModule } from '../../shared/shared.module';
 import { HeaderComponent } from './components/header/header.component';
 import { CoreRoutingModule } from './core.routing.module';
+import { DashboardModule } from '../dashboard/dashboard.module';
 
 @NgModule({
   declarations: [CoreComponent, HeaderComponent],
   exports: [CoreComponent, HeaderComponent],
-  imports: [CommonModule, SharedModule, CoreRoutingModule],
+  imports: [CommonModule, SharedModule, CoreRoutingModule, DashboardModule],
 })
 export class CoreModule {}
