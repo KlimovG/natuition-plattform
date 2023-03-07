efb74b8734ba26476adea0651cb03dbfa674e1ae
Georgii Klimov <klimovgg@gmail.com>
2023-03-07 06:26:44 +0100

    fix styling

--- src/app/modules/sessions/components/sessions/sessions.component.ts
+++ src/app/modules/sessions/components/sessions/sessions.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IButtonsData } from '../../../../shared/components/buttons-list/buttons-list.component';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
@@ -19,33 +19,32 @@ import { faDownload } from '@fortawesome/free-solid-svg-icons';
       (onClick)="onSessionClick.emit($event)"
     >
     </app-buttons-list>
-    <div class="w-full pt-5 ">
-      <app-button-main
-        css="bg-primary-main text-white lg:hover:bg-white lg:hover:text-primary-main border border-primary-main"
-        [fullWidth]="true"
-        (click)="onMoreClick.emit()"
-      >
-        {{ 'sessions.more' | translate }}
-        <fa-icon [icon]="icon" [fixedWidth]="true"></fa-icon>
-      </app-button-main>
-    </div>
+
     <app-spinner
       name="sessionList"
       [showSpinner]="isLoading"
       size="large"
     ></app-spinner>
 
+    <div class="w-full pt-5 mt-auto">
+      <app-button-main
+        css="bg-primary-main text-white lg:hover:bg-white lg:hover:text-primary-main border border-primary-main"
+        [fullWidth]="true"
+        (click)="onMoreClick.emit($event)"
+      >
+        {{ 'sessions.more' | translate }}
+        <fa-icon [icon]="icon" [fixedWidth]="true"></fa-icon>
+      </app-button-main>
+    </div>
+
     <ng-content></ng-content>
   `,
 })
-export class SessionsComponent implements OnInit {
+export class SessionsComponent {
   @Input() sessions: IButtonsData[];
   @Input() activeSession: string;
   @Input() isLoading: boolean;
   @Output() onSessionClick = new EventEmitter<string>();
   @Output() onMoreClick = new EventEmitter<any>();
-  constructor() {}
   icon = faDownload;
-
-  ngOnInit(): void {}
 }
