72f590e01e9ee6f997511fcecf34af915ffb0ec3
Georgii Klimov <klimovgg@gmail.com>
2023-03-16 22:51:20 +0100

    add action to update the status

--- src/app/modules/robots/state/robots.actions.ts
+++ src/app/modules/robots/state/robots.actions.ts
@@ -2,9 +2,21 @@ import { Action } from '@ngrx/store';
 import { RobotModel } from '../models/robot.model';
 
 export enum RobotsActionTypes {
+  UPDATE_ROBOT_STATUS = '[Robots] Update robot status',
+  UPDATE_ROBOT_STATUS_RESPONSE = '[Robots] Update robot status: response',
   GET_ROBOTS_CUSTOMER = '[Robots] Get robots for customer',
   GET_ROBOTS_CUSTOMER_SUCCESS = '[Robots] Get robots for customer success',
   SET_ACTIVE_ROBOT = '[Robots] Set active robot',
+}
+
+export class UpdateRobotStatus implements Action {
+  readonly type = RobotsActionTypes.UPDATE_ROBOT_STATUS;
+}
+
+export class UpdateRobotStatusResponse implements Action {
+  readonly type = RobotsActionTypes.UPDATE_ROBOT_STATUS_RESPONSE;
+
+  constructor(public payload: number) {}
 }
 
 export class GetRobotsForCustomer implements Action {
@@ -26,6 +38,8 @@ export class SetActiveRobot implements Action {
 }
 
 export type RobotsActionUnion =
+  | UpdateRobotStatus
+  | UpdateRobotStatusResponse
   | SetActiveRobot
   | GetRobotsForCustomer
   | GetRobotsForCustomerSuccess;
