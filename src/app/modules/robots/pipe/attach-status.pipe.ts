781e6c579a908462f3cd6bae28f781a133716ce9
Georgii Klimov <klimovgg@gmail.com>
2023-08-25 01:29:56 +0200

    add pipe to attach status

---
+++ src/app/modules/robots/pipe/attach-status.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { RobotModel } from '../models/robot.model';
+import { RobotsService } from '../service/robots.service';
+
+@Pipe({
+  name: 'attachStatus',
+})
+export class AttachStatusPipe implements PipeTransform {
+  constructor(private socketService: RobotsService) {}
+
+  transform(robots: RobotModel[]): RobotModel[] {
+    return robots.map((robot) => {
+      this.socketService.registerRobot(robot.serial);
+      const status$ = this.socketService.getRobotStatus(robot.serial);
+      return { ...robot, status$ };
+    });
+  }
+}
