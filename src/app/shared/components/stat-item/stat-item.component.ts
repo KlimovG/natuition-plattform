2c8210dff366a4cbab4032c12c9f2159b0671cbb
Georgii Klimov <klimovgg@gmail.com>
2023-09-27 20:35:31 +0200

    fix: styling for stat boxes

--- src/app/shared/components/stat-item/stat-item.component.ts
+++ src/app/shared/components/stat-item/stat-item.component.ts
@@ -4,7 +4,9 @@ import { Component, Input } from '@angular/core';
   selector: 'app-stat-item',
   template: `
     <div class="flex justify-end gap-4 w-full HindMadurai-Light items-center">
-      <h4 class=" lg:w-1/2  text-right noselect">{{ label | translate }}</h4>
+      <h4 class="lg:w-1/2 w-1/4  text-right noselect">
+        {{ label | translate }}
+      </h4>
       <div
         class="border-gray-700 border-2 rounded mr-0 py-2 px-2 lg:w-1/2 max-w-stat-box w-3/4 text-center "
       >
